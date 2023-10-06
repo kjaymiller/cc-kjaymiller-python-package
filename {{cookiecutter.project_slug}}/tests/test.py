6a0f4602c777da4e96f0f8aaef97da0f5b988628
Jay Miller <kjaymiller@gmail.com>
2023-10-06 14:22:44 -0400

    update test.py

--- {{cookiecutter.project_slug}}/tests/test.py
+++ {{cookiecutter.project_slug}}/tests/test.py
@@ -1,3 +1,3 @@
-from {{cookiecutter.package_name}} import {{cookiecutter.package_name}
+from {{cookiecutter.package_name}} import {{cookiecutter.package_name}}
 # import pytest
 # from pathlib import Path
